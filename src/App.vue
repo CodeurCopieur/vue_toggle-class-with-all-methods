5bbf3cd1645ba268a2fcd3a61e28a7c9b0d37676
codwerk <louis_widdy@live.fr>
2020-10-14 21:22:26 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <button :class="{ selected : isSelected }" @click="isSelected = !isSelected">bouton 1</button>
+      <button @click="toggle">bouton 2</button>
+      <button @click="toggle($event, selected)">bouton 3</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      isSelected: false
+    }
+  },
+  methods: {
+    toggle(event) {
+      event.target.classList.toggle('selected');
+    },
+    toggleAdvanced(event, theClass){
+      event.target.classList.toggle(theClass);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/*===== BASE =====*/
+*,::before,::after{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background-color: #EEE;
+  font-family: sans-serif;
+}
+
+.container {
+  margin: 20px auto;
+  width: 1200px;
+  padding: 20px;
+  display: flex;
+  justify-content: space-around;
+}
+
+.container button {
+  padding: 10px 20px;
+  background-color: red;
+  color: #fff;
+  border: 1px solid red;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.container button:focus {
+  outline: none;
+}
+
+.container button.selected {
+  background-color: green;
+  border: 1px solid green;
 }
 </style>
